63ef2b3ef58be43a76e565b1bd39518d263ddaf1
kanakanho <kahoshibakichi@gmail.com>
2024-01-03 03:31:34 +0900

    Revert "fix: デプロイ用のコンフィグ"
    This reverts commit 19191b09de8892faee1b5d9b0b57908bcebdb3a6.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    // github pages
+    base: process.env.GITHUB_PAGES ? "/munsell-colors/" : "./",
     plugins: [react()],
-    base: '/munsell-colors/'
 });

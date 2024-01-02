5bb240fe6b8d208139e0c7509b797dfcb436f78e
kanakanho <kahoshibakichi@gmail.com>
2024-01-03 03:38:26 +0900

    fix: vite config base name

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
     // github pages
-    base: process.env.GITHUB_PAGES ? "/munsell-colors/" : "./",
+    base: process.env.GITHUB_PAGES ? "munsell-colors" : "./",
     plugins: [react()],
 });

fd2ce6295d98cd153c5fdc01e11c11e097aea3eb
kanakanho <kahoshibakichi@gmail.com>
2024-01-03 03:12:06 +0900

    fix: githubにデプロイするための用意

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    // github pages
+    base: process.env.GITHUB_PAGES ? "/munsell-colors/" : "./",
+    plugins: [react()],
+});

499e479727d067bfc69cb9dc598a6e024649c95d
kanakanho <kahoshibakichi@gmail.com>
2024-01-03 19:41:54 +0900

    Revert "Merge pull request #6 from kanakanho/add-chose-mode"
    This reverts commit 52fa66c878d16d6c8783f65e1561a66c4a769f1e, reversing
    changes made to 4e24c1ec807e9c67b4636ce6c64ec839dc512258.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import { FC } from "react";
 import Layout from "./component/layout/Layout";
-import { RecoilRoot } from "recoil";
 
 const App: FC = () => {
     return (
-        <RecoilRoot>
+        <>
             <Layout />
-        </RecoilRoot>
+        </>
     );
 };
 
